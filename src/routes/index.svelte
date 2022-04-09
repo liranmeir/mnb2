52d41fd449b6c5a5b4295d9f020e3a119259c047
liranmeir <liranmeir@gmail.com>
2022-04-09 23:48:26 +0300

    adding tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,21 +11,10 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
+	<h1 class="text-3xl font-bold underline">
+		MNB
+	  </h1>
 	<Counter />
 </section>
 
@@ -40,20 +29,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
